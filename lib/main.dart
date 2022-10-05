d9c4d570555ed4305789fd2d1c17c78b4d04847d
Qusyairi Othman <qusyairi.work@gmail.com>
2022-10-05 15:48:32 +0800

    Chat

--- lib/main.dart
+++ lib/main.dart
@@ -3,38 +3,81 @@ import 'package:web_socket_channel/io.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    const title = 'WebSocket Demo';
+    return const MaterialApp(
+      title: title,
+      home: MyHomePage(
+        title: title,
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required String title}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
   // Localhost for android — 10.0.2.2
   // Localhost for iOS — 127.0.0.1
   final IOWebSocketChannel channel = IOWebSocketChannel.connect(
       'ws://10.0.2.2:8080',
       protocols: ['echo-protocol']);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: StreamBuilder(
-        stream: channel.stream,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              return const Text('No connection');
-            case ConnectionState.waiting:
-              return const Text('Connected');
-            case ConnectionState.active:
-              return Text('${snapshot.data}');
-            case ConnectionState.done:
-              return Text('${snapshot.data} (closed)');
-          }
-        },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("demo"),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Form(
+              child: TextFormField(
+                controller: _controller,
+                decoration: const InputDecoration(labelText: 'Send a message'),
+              ),
+            ),
+            const SizedBox(height: 24),
+            StreamBuilder(
+              stream: channel.stream,
+              builder: (context, snapshot) {
+                return Text(snapshot.hasData ? '${snapshot.data}' : '');
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sendMessage,
+        tooltip: 'Send message',
+        child: const Icon(Icons.send),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      channel.sink.add(_controller.text);
+    }
+  }
+
+  @override
+  void dispose() {
+    channel.sink.close();
+    _controller.dispose();
+    super.dispose();
   }
 }
